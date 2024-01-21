7bf3497003a255efba2fe39fe2686d9375f47d0b
fumikito <guy@hametuha.com>
2024-01-21 21:20:35 +0900

    Fix JS lint error.

--- src/js/app.js
+++ src/js/app.js
@@ -2,16 +2,19 @@
  * Search form result.
  */
 import React from 'react';
-import { SearchForm } from "./components/search-form";
+import { SearchForm } from './components/search-form';
+// eslint-disable-next-line react/no-deprecated
 import { render } from 'react-dom';
-import {Ticket} from "./components/ticket-page";
+import { Ticket } from './components/ticket-page';
 
 const form = document.getElementById( 'search-form' );
 if ( form ) {
-  render( <SearchForm />, form );
+	// eslint-disable-next-line react/no-deprecated
+	render( <SearchForm />, form );
 }
 
 const ticketWrapper = document.getElementById( 'ticket' );
 if ( ticketWrapper ) {
-  render( <Ticket id={ ticketWrapper.dataset.ticketId } />, ticketWrapper );
+	// eslint-disable-next-line react/no-deprecated
+	render( <Ticket id={ ticketWrapper.dataset.ticketId } />, ticketWrapper );
 }
--- src/js/components/helper.js
+++ src/js/components/helper.js
@@ -3,17 +3,17 @@
  */
 
 export function fetchApi( path, init = {} ) {
-  const url = document.location.protocol + '//' + document.location.host + path;
-  const options = {
-    credentials: 'include',
-  };
-  for ( let prop in init ) {
-    if ( ! init.hasOwnProperty( prop ) ) {
-      continue;
-    }
-    options[ prop ] = init[ prop ];
-  }
+	const url =
+		document.location.protocol + '//' + document.location.host + path;
+	const options = {
+		credentials: 'include',
+	};
+	for ( const prop in init ) {
+		if ( ! init.hasOwnProperty( prop ) ) {
+			continue;
+		}
+		options[ prop ] = init[ prop ];
+	}
 
-
-  return fetch( url, options );
+	return fetch( url, options );
 }
